b47cbdda822db7dfd229483602672f11c2bff74b
Alexey <info@createit.ru>
2024-03-11 15:42:59 +0300

    Добавлен объект msExtraField

---
+++ core/components/minishop3/src/Model/msExtraField.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MiniShop3\Model;
+
+use xPDO\Om\xPDOSimpleObject;
+
+/**
+ * Class msExtraField
+ *
+* @property string $class
+* @property string $key
+* @property string $label
+* @property string $dbtype
+* @property string $precision
+* @property string $phptype
+* @property bool $null
+* @property string $default
+* @property string $attributes
+* @property bool $active
+ *
+ * @package MiniShop3\Model
+ */
+class msExtraField extends xPDOSimpleObject
+{
+    
+}
