adc9d0d0a7c335703b88b512a4a49d09c5c673c0
YanaStyopochkina <managerproalmaty@gmail.com>
2024-11-20 16:12:01 +0500

    Вернула как было

--- assets/components/minishop3/action.php
+++ assets/components/minishop3/action.php
@@ -2,15 +2,12 @@
 
 if (empty($_REQUEST['action']) && empty($_REQUEST['ms3_action'])) {
     http_response_code(403);
-    echo "Ошибка: отсутствуют необходимые параметры.";
-} else {
-    // Сообщение об успешной отправке
-    echo "Данные отправлены!";
 }
 
 if (!empty($_REQUEST['action'])) {
     $_REQUEST['ms3_action'] = $_REQUEST['action'];
 }
+
 
 /** @noinspection PhpIncludeInspection */
 require dirname(__FILE__, 4) . '/index.php';
