122f6fe3d45053405494625434905030d1aefb8b
YanaStyopochkina <managerproalmaty@gmail.com>
2024-11-20 16:06:43 +0500

    Comment

--- assets/components/minishop3/action.php
+++ assets/components/minishop3/action.php
@@ -2,6 +2,10 @@
 
 if (empty($_REQUEST['action']) && empty($_REQUEST['ms3_action'])) {
     http_response_code(403);
+    echo "Ошибка: отсутствуют необходимые параметры.";
+} else {
+    // Сообщение об успешной отправке
+    echo "Данные отправлены!";
 }
 
 if (!empty($_REQUEST['action'])) {
