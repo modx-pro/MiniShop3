0f7e064cbeb8709ca0e4d84661b059d329348c46
biz87 <team@tolkit.top>
2026-04-20 17:03:26 +0500

    chore(release): v1.10.0-beta1 — options Vue refactor
    Release notes for the full Vue migration of product options management.

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -12,7 +12,7 @@ return [
     'name' => 'MiniShop3',
     'name_lower' => 'minishop3',
     'name_short' => 'ms3',
-    'version' => '1.9.0',
+    'version' => '1.10.0',
     'release' => 'beta1',
     // Install package to site right after build
     'install' => false,
