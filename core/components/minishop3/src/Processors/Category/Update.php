10c0b3115f9ecda4ab5080140b08f44e6ae8ba2d
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-03 19:30:22 +0600

    fix(resource): allow modDocument to msProduct type conversion on update
    Product Update processor failed to load the resource when class_key was
    changed in settings because parent initialize() requires msProduct in DB.
    Extract EnsureTargetClassKeyTrait from Category/Update and reuse in
    Product/Update. Closes #329.

--- core/components/minishop3/src/Processors/Category/Update.php
+++ core/components/minishop3/src/Processors/Category/Update.php
@@ -2,39 +2,19 @@
 
 namespace MiniShop3\Processors\Category;
 
-use MODX\Revolution\Processors\Resource\Update as UpdateProcessor;
 use MiniShop3\Model\msCategory;
-use MODX\Revolution\modResource;
+use MiniShop3\Processors\Resource\EnsureTargetClassKeyTrait;
+use MODX\Revolution\Processors\Resource\Update as UpdateProcessor;
 
 class Update extends UpdateProcessor
 {
+    use EnsureTargetClassKeyTrait;
+
     public $classKey = msCategory::class;
     public $languageTopics = ['resource', 'minishop3:default'];
     public $permission = 'mscategory_save';
     public $beforeSaveEvent = 'OnBeforeDocFormSave';
     public $afterSaveEvent = 'OnDocFormSave';
-
-    /**
-     * @return bool|null|string
-     */
-    public function initialize()
-    {
-        $primaryKey = $this->getProperty($this->primaryKeyField, false);
-        if (empty($primaryKey)) {
-            return $this->modx->lexicon($this->classKey . '_err_ns');
-        }
-
-        if (!$this->modx->getCount($this->classKey, ['id' => $primaryKey, 'class_key' => $this->classKey])) {
-            $res = $this->modx->getObject(modResource::class, ['id' => $primaryKey]);
-            if ($res) {
-                $res->set('class_key', $this->classKey);
-                $res->save();
-            }
-        }
-
-        return parent::initialize();
-    }
-
 
     /**
      * @return int|mixed|string
