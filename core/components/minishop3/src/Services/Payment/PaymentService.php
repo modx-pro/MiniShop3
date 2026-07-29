226feb781ef781f841dbd21cff3d45a97b975cd8
Ivan Bochkarev <ivanx86@gmail.com>
2026-07-29 17:57:28 +0600

    fix(ci): drop loadCustomClasses, obsolete payment-link test, baseline

--- core/components/minishop3/src/Services/Payment/PaymentService.php
+++ core/components/minishop3/src/Services/Payment/PaymentService.php
@@ -55,10 +55,6 @@ class PaymentService
             $class = $this->defaultControllerClass;
         }
 
-        if ($this->ms3) {
-            $this->ms3->loadCustomClasses('payment');
-        }
-
         try {
             $controller = new $class($this->ms3, []);
 
